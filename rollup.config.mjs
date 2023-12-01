6c1254a300730431a1a2f940df7cf6d796114255
blacklightburn <nadymorigin@gmail.com>
2023-12-01 15:48:41 +0300

    fix: rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,23 @@
+import postcss from 'rollup-plugin-postcss';
+import typescript from '@rollup/plugin-typescript';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+  input: 'src/index.tsx',
+  output: {
+    dir: 'dist',
+    format: 'esm',
+    sourcemap: true,
+  },
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    typescript(),
+    commonjs(),
+    postcss({
+      modules: true,
+    }),
+  ],
+};
