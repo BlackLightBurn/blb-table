ab21d64e069ebd96a97768f1926c2d3e91467452
blacklightburn <nadymorigin@gmail.com>
2023-12-07 14:19:52 +0300

    fix: export

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
-export { Table, ShowMore } from './Table';
+export { Table } from './Table';
+export { ShowMore } from './Table';
 export type { QueryParams } from './types';
 export type { ColumnDef } from '@tanstack/react-table';
